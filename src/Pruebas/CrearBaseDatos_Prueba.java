b1f1965ded7f813b03c02b45c1eff46ac33fdd7f
APachon10 <Pachondestroy95@gmail.com>
2018-11-26 15:36:57 +0100

    a

--- src/Pruebas/CrearBaseDatos_Prueba.java
+++ src/Pruebas/CrearBaseDatos_Prueba.java
@@ -1,6 +1,5 @@
 package Pruebas;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -17,8 +16,8 @@ public class CrearBaseDatos_Prueba {
 		String dbName = "BaseDatosApacheDerby\\ForHonor";
 		String dbParam = "create=true"; //la base de datos se creará si no existe todavía
 		
-		//pv.crearBaseDatos(dbName,dbParam);
-		//pv.insertarDatos(dbName, dbParam);
+		pv.crearBaseDatos(dbName,dbParam);
+		pv.insertarDatos(dbName, dbParam);
 		pv.realizarConsultas(dbName, dbParam);
 	}
 	public void crearBaseDatos(String dbName,String dbParam) {
@@ -32,7 +31,7 @@ public class CrearBaseDatos_Prueba {
 			String crearTablaFacciones ="CREATE TABLE Faccion("+
 					"faccion_id  INTEGER NOT NULL CONSTRAINT faccion_id PRIMARY KEY,"+
 					"nombre_faccion VARCHAR(15),"+ 
-					"lore VARCHAR(100)"+
+					"lore VARCHAR(150)"+
 				    ")";
 			st.execute(crearTablaFacciones);
 			System.out.println("Tabla Facciones Creada");
@@ -47,8 +46,7 @@ public class CrearBaseDatos_Prueba {
 				    ")";
 			st.execute(crearTablaPersonajes);
 			System.out.println("Tabla Personajes creada ");
-			
-			
+			System.out.println("-----------------------------------");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -62,13 +60,9 @@ public class CrearBaseDatos_Prueba {
 
 		  Statement st = conn.createStatement();
 		  //Insertamos los valores dentro del campo Facciones 
-		  st.executeUpdate("INSERT INTO Faccion(faccion_id,nombre_faccion, lore) VALUES(1,'Caballeros','son paradigmas del poder.La Legion de Hierro los envio para llevar la paz a esas tierras')");
-		  
-		  st.executeUpdate("INSERT INTO Faccion(faccion_id,nombre_faccion, lore) VALUES(2,'Vikingos',"
-		  + "'Los vikingos desaparecieron hace siglos, tras escapar de sus destrozadas tierras natales en pos de costas desconocidas.");
-		  
-		  st.executeUpdate("INSERT INTO Faccion(faccion_id,nombre_faccion, lore) VALUES(3,'Samurais',"
-		  + "'Originarios de una tienda allende los mares,cuentan la historia de un emperador y una patria que desaparecieron entre el mar y el fuego.')");
+		  st.executeUpdate("INSERT INTO Faccion(faccion_id,nombre_faccion,lore) VALUES(1,'Caballeros','son paradigmas del poder.La Legion de Hierro los envio para llevar la paz a esas tierras')");
+		  st.executeUpdate("INSERT INTO Faccion(faccion_id,nombre_faccion,lore) VALUES(2,'Vikingos','Los vikingos desaparecieron hace siglos,tras escapar de sus destrozadas tierras natales en pos de costas desconocidas')");
+		  st.executeUpdate("INSERT INTO Faccion(faccion_id,nombre_faccion,lore) VALUES(3,'Samurais','Originarios de una tienda allende los mares,cuentan la historia de un emperador y una patria que desaparecieron entre el mar y el fuego.')");
 		  
 		  System.out.println("Se han insertado todos  los registros dentro de la tabla Facciones ");
 
@@ -100,24 +94,25 @@ public class CrearBaseDatos_Prueba {
 		  /*Consultas para la tabla Faccion*/
 		  ResultSet rs=st.executeQuery("SELECT * FROM Faccion");
 		  
-		  
 		  while (rs.next()){
 		    Integer faccion_id = rs.getInt("faccion_id");
 		    String name_faction = rs.getString("nombre_faccion");
 		    String history = rs.getString("lore");
 		    
 		    System.out.println("faccion_id:" +faccion_id+"\nNombre Faccion: "+name_faction+"\nLore: " +history); 
+		    System.out.println("---------------------------------------------");
 		  }
 		  ResultSet rs2=st.executeQuery("SELECT * FROM Personajes");
 		  /*Consultas para la tabla Personajes */
 		  while (rs2.next()){
-		    Integer pj_id = rs.getInt("personaje_id");
-		    String name_pj = rs.getString("nombre_personaje");
-		    Integer atack = rs.getInt("Ataque");
-		    Integer def = rs.getInt("Defensa");
-		    Integer faction_id2 = rs.getInt("faccion_id");
+		    Integer pj_id = rs2.getInt("personaje_id");
+		    String name_pj = rs2.getString("nombre_personaje");
+		    Integer atack = rs2.getInt("Ataque");
+		    Integer def = rs2.getInt("Defensa");
+		    Integer faction_id2 = rs2.getInt("faccion_id");
 		    
-		    System.out.println("Personaje_id:"+pj_id+"\n Nombre Personaje:"+name_pj+"\n Ataque:" +atack + "\nDefensa:"+def+"\nfaction_id "+faction_id2); 
+		    System.out.println("Personaje_id:"+pj_id+"\n Nombre Personaje:"+name_pj+"\n Ataque:" +atack + "\nDefensa:"+def+"\nfaction_id "+faction_id2);
+		    System.out.println("---------------------------------------------");
 		  }
 		  
 		  //Cerramos los 2 resulSet
