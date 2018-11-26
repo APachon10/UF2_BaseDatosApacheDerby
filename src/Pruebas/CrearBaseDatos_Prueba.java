afdfa0c44058292da12d9fcfb24cce55cd5e11e4
APachon10 <Pachondestroy95@gmail.com>
2018-11-26 12:52:03 +0100

    Optimizacion creacion

--- src/Pruebas/CrearBaseDatos_Prueba.java
+++ src/Pruebas/CrearBaseDatos_Prueba.java
@@ -8,14 +8,14 @@ import java.sql.Statement;
 public class CrearBaseDatos_Prueba {
 	public static void main(String[] args) {
 		CrearBaseDatos_Prueba pv = new CrearBaseDatos_Prueba();
-		pv.crearBaseDatos();
-	}
-	public void crearBaseDatos() {
+		
 		String driver = "org.apache.derby.jdbc.EmbeddedDriver";
-
 		String dbName = "BaseDatosApacheDerby\\ForHonor";
 		String dbParam = "create=true"; //la base de datos se creará si no existe todavía
-
+		
+		pv.crearBaseDatos(dbName,dbParam);
+	}
+	public void crearBaseDatos(String dbName,String dbParam) {
 		String connectionURL = "jdbc:derby:" + dbName + ";" + dbParam;
 		Connection conn = null;
 		try {
@@ -42,6 +42,31 @@ public class CrearBaseDatos_Prueba {
 
 	}
 	public void realizarConsulta() {
+		String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+		String dbName = "/DerbyDB/ExampleDB";
+		String connectionURL = "jdbc:derby:" + dbName;
+		Connection conn = null;
 
+		try{
+		  Class.forName(driver);
+		} catch(java.lang.ClassNotFoundException e) {
+		  e.printStackTrace();
+		}
+
+		try {
+		  conn = DriverManager.getConnection(connectionURL);
+
+		  Statement st = conn.createStatement();
+		  st.executeUpdate("INSERT INTO users VALUES('Jose', 'Olmedo', 1)");
+		  st.executeUpdate("INSERT INTO users VALUES('Maria', 'Hoz', 2)");
+
+		  System.out.println("Se han insertado dos registros");
+		} catch (Throwable e)  {
+		  System.out.println("Ha fallado la insercion de dos registros");
+		  e.printStackTrace();
+		} finally {
+		  try { conn.close(); }
+		  catch (Throwable t){}
+		}
 	}
 }
