186b65c44f67196f70d3d1876a1177752973dd1a
APachon10 <Pachondestroy95@gmail.com>
2018-11-26 11:23:31 +0100

    Metodos para realizar consultas creados

--- src/Pruebas/CrearBaseDatos_Prueba.java
+++ src/Pruebas/CrearBaseDatos_Prueba.java
@@ -1,5 +1,16 @@
 package Pruebas;
 
 public class CrearBaseDatos_Prueba {
-
+	public static void main(String[] args) {
+		
+	}
+	public void crearBaseDatos() {
+		
+	}
+	public void Menu() {
+		
+	}
+	public void realizarConsulta() {
+		
+	}
 }
