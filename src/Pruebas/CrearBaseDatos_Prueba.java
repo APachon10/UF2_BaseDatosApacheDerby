87623078c82fbe36558767d707e8003eb476f197
APachon10 <Pachondestroy95@gmail.com>
2018-11-26 13:40:15 +0100

    Bsae de Datos creada Correctamente

--- src/Pruebas/CrearBaseDatos_Prueba.java
+++ src/Pruebas/CrearBaseDatos_Prueba.java
@@ -16,7 +16,8 @@ public class CrearBaseDatos_Prueba {
 		String dbParam = "create=true"; //la base de datos se creará si no existe todavía
 		
 		pv.crearBaseDatos(dbName,dbParam);
-		pv.insertarDatos(dbName, dbParam);
+		//pv.insertarDatos(dbName, dbParam);
+		//pv.realizarConsultas(dbName, dbParam);
 	}
 	public void crearBaseDatos(String dbName,String dbParam) {
 		String connectionURL = "jdbc:derby:" + dbName + ";" + dbParam;
@@ -27,22 +28,24 @@ public class CrearBaseDatos_Prueba {
 
 			Statement st = conn.createStatement();
 			String crearTablaFacciones ="CREATE TABLE Faccion("+
-					"faccion_id INTEGER NOT NULL CONSTRAINT faccion_id PRIMARY KEY,"+
+					"faccion_id  INTEGER NOT NULL CONSTRAINT faccion_id PRIMARY KEY,"+
 					"nombre_faccion VARCHAR(15),"+ 
 					"lore VARCHAR(20)"+
 				    ")";
 			st.execute(crearTablaFacciones);
+			System.out.println("Tabla Facciones Creada");
+			
 			String crearTablaPersonajes ="CREATE TABLE Personajes("+
 					"personaje_id INTEGER NOT NULL CONSTRAINT personaje_id PRIMARY KEY,"+
 					"nombre_personaje VARCHAR(15),"+ 
-					"Ataque INTEGER"+ 
-					"Defensa INTEGER"+ 
-					"faccion_id INTEGER"+ 
+					"Ataque INTEGER,"+ 
+					"Defensa INTEGER,"+ 
+					"faccion_id INTEGER,"+ 
 					"FOREIGN KEY(faccion_id) REFERENCES Faccion(faccion_id)"+
 				    ")";
 			st.execute(crearTablaPersonajes);
-
-			System.out.println("Tabla Facciones Creada");
+			System.out.println("Tabla Personajes creada ");
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -101,19 +104,16 @@ public class CrearBaseDatos_Prueba {
 		  conn = DriverManager.getConnection(connectionURL);
 
 		  Statement st = conn.createStatement();
-		  ResultSet rs=st.executeQuery("SELECT * FROM users");
+		  ResultSet rs=st.executeQuery("SELECT * FROM Facciones");
 		  while (rs.next()){
-		    Integer idUser = rs.getInt("idUser");
-		    String first = rs.getString("FirstName");
-		    String last = rs.getString("LastName");
+		    Integer idUser = rs.getInt("faccion_id");
+		    String first = rs.getString("nombre_faccion");
+		    String last = rs.getString("lore");
 		    System.out.println(idUser + " se llama: " + first + " " + last);
 		  }
 		  rs.close();
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-	public void Menu() {
-
 	}
 }
